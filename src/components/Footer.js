978888f2d26598a32fd12184d0bbe3d224abdc5a
jymmak <henryfrancisco_fernando@hotmail.com>
2018-03-03 18:30:52 -0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-
+import facebook from './images/facebook.png';
+import twitter from './images/twitter.png';
+import linkedln from './images/linkedln.png';
+import git from './images/git.png';
 import './css/footer.css'
 
 class Footer extends Component {
   render() {
     return (
-      <div className="h40">
-       
+      <div className='foot text-center' >
+        <img className='facebook' src={facebook} alt='facebook'></img>
+        <img className='twitter' src={twitter} alt='twitter'></img>
+        <img className='linkedln' src={linkedln} alt='linkedln'></img>
+        <img className='git' src={git} alt='git'></img>
+        <p>&#9400; LabCar technologies Inc.</p>
       </div>
     );
   }
